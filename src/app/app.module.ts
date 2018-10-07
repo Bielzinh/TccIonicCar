c4ac1769f7811e3d011593afb86083f42dadc15a
Gabriel Fernando <biel.nrg@gmail.com>
2018-10-06 21:45:12 -0300

    Criando Menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,9 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { LoginPage } from '../pages/login/login';
 //Plugindd
 import { IonicStorageModule } from '@ionic/storage';
+import { MyApp } from './app.component';
 //firebase config
 import { firebaseConfig } from '../configs/firebase';
 import { AngularFireModule } from 'angularfire2';
@@ -19,29 +17,36 @@ import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/dat
 //providers
 import { AuthProvider } from '../providers/auth';
 import {FirebaseProvider} from '../providers/firebase';
+//pages
+import { LoginPageModule } from '../pages/login/login.module';
+import { HomePageModule } from '../pages/home/home.module';
+
+
+
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage,
-    LoginPage
+    MyApp
   ],
   imports: [
+    //pages
+    LoginPageModule,
+    HomePageModule,
+
+    //outros
     AngularFireModule.initializeApp(firebaseConfig),
-    BrowserModule,
-    BrowserAnimationsModule,
-    IonicModule.forRoot(MyApp),
-    IonicStorageModule.forRoot(),
     AngularFirestoreModule,
     AngularFireDatabaseModule,
     AngularFireAuthModule,
     BrowserAnimationsModule,
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage,
-    LoginPage
+    MyApp
+
   ],
   providers: [
     AngularFireDatabase,
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,14 +1,51 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import {trigger, style, animate, transition } from '@angular/animations';
 import { AuthProvider } from '../../providers/auth';
 import { FirebaseProvider } from '../../providers/firebase';
 import { Storage } from '@ionic/storage';
-
 
 @IonicPage()
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
+  animations: [
+    trigger(
+      'login', [
+        transition(':enter', [
+          style({
+            opacity: 0
+          }),
+          animate("1s ease-in-out", style({
+            opacity: 1
+          }))
+        ]),
+        transition(':leave', [
+          style({
+            opacity: 0
+          })
+        ])
+      ],
+    ),
+    trigger(
+      'registro', [
+        transition(':enter', [
+          style({
+            opacity: 0
+          }),
+          animate("1s ease-in-out", style({
+            opacity: 1
+          }))
+        ]),
+        transition(':leave', [
+          style({
+            opacity: 0
+          })
+        ])
+      ],
+    ),
+  ]
+
 })
 export class LoginPage {
 
@@ -25,19 +62,16 @@ registerForm = {
   password: '',
   name: ''
 }
-
-  constructor(public navCtrl: NavController,
+  constructor(
+    public navCtrl: NavController,
      public navParams: NavParams,
    private authProvider: AuthProvider,
    private firebaseProvider: FirebaseProvider,
    private loadingCtrl: LoadingController,
    private storage: Storage
-
-
-    ) {
+ ) {
   }
-
-  //exibir formulário de registro
+//exibir formulário de registro
 exibirRegistrar(){
   this.login = false;
   this.register = true;
@@ -48,65 +82,62 @@ exibirLogin(){
   this.login = true;
   this.register = false;
 }
-
 //login
 Fazerlogin(){
-  let load = this.loadingCtrl.create();
-  load.present();
+let load = this.loadingCtrl.create();
+load.present();
 
-  this.authProvider.login(this.loginForm)
+this.authProvider.login(this.loginForm)
+.then((res) => {
+  let uid = res.user.uid;
+  this.firebaseProvider.getUser(uid)
   .then((res) => {
-    let uid = res.user.uid;
-    this.firebaseProvider.getUser(uid)
-    .then((res) => {
-      let data = res.data();
+    let data = res.data();
+    this.storage.set('usuario', data)
+    .then(() => {
+      load.dismiss();
+      this.navCtrl.setRoot('HomePage');
+    })
+  })
+})
+.catch((err) => {
+  load.dismiss();
+
+})
+}
+//REGISTRO
+criarNovaConta(){
+let load = this.loadingCtrl.create();
+load.present();
+
+this.authProvider.register(this.registerForm)
+.then((res) => {
+  let uid = res.user.uid;
+//Organizar Dados
+  let data = {
+    uid: uid,
+    name: this.registerForm.name,
+    email: this.registerForm.email
+  };
+  //Gravar usr no Firestore
+  this.firebaseProvider.postUser(data)
+  .then(() => {
       this.storage.set('usuario', data)
       .then(() => {
         load.dismiss();
         this.navCtrl.setRoot('HomePage');
       })
-    })
-  })
-  .catch((err) => {
-    load.dismiss();
+})
+.catch((err) => {
+  load.dismiss();
 
-  })
-  }
-  //REGISTRO
-  criarNovaConta(){
-  let load = this.loadingCtrl.create();
-  load.present();
-
-  this.authProvider.register(this.registerForm)
-  .then((res) => {
-    let uid = res.user.uid;
-  //Organizar Dados
-    let data = {
-      uid: uid,
-      name: this.registerForm.name,
-      email: this.registerForm.email
-    };
-    //Gravar usr no Firestore
-    this.firebaseProvider.postUser(data)
-    .then(() => {
-        this.storage.set('usuario', data)
-        .then(() => {
-          load.dismiss();
-          this.navCtrl.setRoot('HomePage');
-        })
-  })
-  .catch((err) => {
-    load.dismiss();
-
-  })
-  })
-  .catch((err) => {
-    load.dismiss();
+})
+})
+.catch((err) => {
+  load.dismiss();
 
 
-  })
+})
 
-  }
-
-
+}
 }
